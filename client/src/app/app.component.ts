fbf05476488e57bc7b75ef968d7fd862392b9d0a
Shyam Menon <shyam.s.menon@gmail.com>
2020-09-30 17:05:49 +0530

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,3 @@
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,18 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit { 
+export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-     this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
   }
 }
---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,100 @@
+import { ShopParams } from './../shared/models/shopParams';
+import { IType } from './../shared/models/productType';
+import { IBrand } from './../shared/models/brands';
+import { ShopService } from './shop.service';
+import { IProduct } from '../shared/models/product';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search', {static: true}) searchTerm: ElementRef;
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  shopParams = new ShopParams();
+  totalCount: number;
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'}
+  ];
+
+  constructor(private shopService: ShopService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(): void {
+    this.shopService.getproducts(this.shopParams).subscribe(response => {
+      this.products = response.data;
+      this.shopParams.pageNumber = response.pageIndex;
+      this.shopParams.pageSize = response.pageSize;
+      this.totalCount = response.count;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getBrands(): void {
+    this.shopService.getBrands().subscribe(response => {
+      // Ability to select all of the brands and reset the brand selected
+      // The spread operator ... on the response takes all of the items in the response
+      // of the API and adds a the object with id 0 and name of 'All' into it
+      this.brands = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getTypes(): void {
+    this.shopService.getTypes().subscribe(response => {
+      this.types = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  onBrandSelected(brandId: number): void{
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number): void{
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string): void{
+    this.shopParams.sort = sort;
+    this.getProducts();
+  }
+
+  onPageChanged(event: any): void{
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch(): void {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onReset(): void {
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+
+}
